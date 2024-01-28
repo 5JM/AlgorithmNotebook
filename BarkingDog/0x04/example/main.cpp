2ea5688231cbd841f92770574af68e5635423265
5jm <toddlf0614@naver.com>
2024-01-28 14:36:52 +0900

    add: 0x04 example

--- BarkingDog/0x04/example/main.cpp
+++ BarkingDog/0x04/example/main.cpp
@@ -1,16 +1,47 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-void printArr(int arr[], int& len){
-    for(int i = 0; i < len; ++i) cout<< arr[i]<<" ";
-    cout<<"\n";
+const int MX = 1000005;
+int dat[MX], pre[MX], nxt[MX];
+int unused = 1;
+
+void traverse(){
+    int cur = nxt[0];
+
+    while(cur != -1){
+        cout<< dat[cur] << " ";
+        cur = nxt[cur];
+    }
+}
+
+void insert(int addr, int num){
+    int postTmpAddr = nxt[addr];
+
+    dat[unused] = num;
+    pre[unused] = addr;
+    nxt[unused] = postTmpAddr;
+
+    if(postTmpAddr != -1) pre[postTmpAddr] = unused;
+    nxt[addr] = unused;
+
+    unused++;
+}
+
+void erase(int addr){
+    int prevTmpAddr = pre[addr];
+    int postTmpAddr = nxt[addr];
+
+    nxt[prevTmpAddr] = postTmpAddr;
+    if(postTmpAddr != -1) pre[postTmpAddr] = prevTmpAddr;
 }
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
+
+    fill(pre, pre+MX, -1);
+    fill(nxt, nxt+MX, -1);
 
     return 0;
 }
