f90a786a95e8d30b959ca1b6aeca582e3b894e09
5jm <toddlf0614@naver.com>
2024-02-04 12:57:50 +0900

    update: example code

--- BarkingDog/0x09/example/main.cpp
+++ BarkingDog/0x09/example/main.cpp
@@ -30,10 +30,12 @@ int main(void){
     queue<pair<int,int> > Q;
 
     vis[0][0] = 1; // (0, 0)을 방문했다고 명시
-    Q.push({0,0}); // 큐에 시작점인 (0, 0)을 삽입.
+    Q.push(make_pair(0, 0)); // 큐에 시작점인 (0, 0)을 삽입.
 
     while(!Q.empty()){
-        pair<int,int> cur = Q.front(); Q.pop();
+        pair<int,int> cur = Q.front(); 
+        Q.pop();
+
         cout << '(' << cur.X << ", " << cur.Y << ") -> ";
 
         for(int dir = 0; dir < 4; dir++){ // 상하좌우 칸을 살펴볼 것이다.
@@ -45,7 +47,7 @@ int main(void){
             if(vis[nx][ny] || board[nx][ny] != 1) continue; // 이미 방문한 칸이거나 파란 칸이 아닐 경우
 
             vis[nx][ny] = 1; // (nx, ny)를 방문했다고 명시
-            Q.push({nx,ny});
+            Q.push(make_pair(nx,ny));
         }
     }
 }
