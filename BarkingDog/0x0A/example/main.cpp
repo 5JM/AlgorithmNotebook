7d6106e5b0fa177bfdb2ad46f8996c0f7de64c8c
5jm <toddlf0614@naver.com>
2024-02-26 15:50:33 +0900

    add: example of lec 0x0A

--- BarkingDog/0x0A/example/main.cpp
+++ BarkingDog/0x0A/example/main.cpp
@@ -1,12 +1,44 @@
 #include <utility>
 #include <iostream>
-#include <queue>
+#include <stack>
+
+#define X first
+#define Y second
 
 using namespace std;
+
+int board[502][502];
+bool vis[502][502];
+
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+int n = 7, m = 10; 
 
 int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
+
+    stack<pair<int, int> > S;
+    vis[0][0] = true;
+    S.push(make_pair(0, 0));
+
+    whlie(!S.empty()){
+        pair<int, int> cur = S.top();
+        S.pop();
+
+        for(int i = 0; i < 4; ++i){
+            int x = cur.X + dx[i];
+            int y = cur.Y + dy[i];
+
+            if(x < 0 || x >= n || y < 0 || y >= m) continue;
+
+            if(board[x][y] != 1 || vis[x][y]) continue;
+
+            S.push(make_pair(x, y));
+            vis[x][y] = true;
+        }
+    }
 
     return 0;
 }
