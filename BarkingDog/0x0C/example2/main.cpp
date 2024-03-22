edb40b9098626d99a2b182279b83bd4e07403c9e
5jm <toddlf0614@naver.com>
2024-03-23 08:40:47 +0900

    add: dummy code of lec 0x17

---
+++ BarkingDog/0x0C/example2/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int n, s, cnt, arr[21];
+
+void func1182(int cur, int tot){
+    if(cur == n){
+        if(tot == s) cnt++;
+        return ;
+    }
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+
+
+    return 0;
+}
